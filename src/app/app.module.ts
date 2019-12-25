d3979d9a11a3393e43c7b432732bd589f128357f
Ken Nguyen <khangnguyen291@gmail.com>
2019-12-25 12:48:24 +1100

    Generate necessary pages and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { DownloadComponent } from './download/download.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent,
+    DownloadComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
